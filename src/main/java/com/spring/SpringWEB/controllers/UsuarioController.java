75f4d2686085571ebeae2f9c284c677c3fc2f3a4
Eladios30 <eladioquien@gmail.com>
2023-06-27 20:51:40 -0700

    Agregandi funcion fecth

--- src/main/java/com/spring/SpringWEB/controllers/UsuarioController.java
+++ src/main/java/com/spring/SpringWEB/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.spring.SpringWEB.controllers;
 
+import com.spring.SpringWEB.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,48 @@ import java.util.List;
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value = "prueba")
-    public List<String> prueba(){
-            return List.of("MAnzana", "Kiwi", "Platano");
+    @RequestMapping(value = "usuario")
+    public Usuario getUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Eladio");
+        usuario.setApellido("Quintero");
+        usuario.setEmail("eladioquien@gmail.com");
+        usuario.setTelefono("6511063209");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario editar(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Eladio");
+        usuario.setApellido("Quintero");
+        usuario.setEmail("eladioquien@gmail.com");
+        usuario.setTelefono("6511063209");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario1")
+    public Usuario ed(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Eladio");
+        usuario.setApellido("Quintero");
+        usuario.setEmail("eladioquien@gmail.com");
+        usuario.setTelefono("6511063209");
+
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuario2")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Eladio");
+        usuario.setApellido("Quintero");
+        usuario.setEmail("eladioquien@gmail.com");
+        usuario.setTelefono("6511063209");
+
+        return usuario;
     }
 }

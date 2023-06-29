bba0879651cdaec83056e80d82e135dedc6fed02
Eladios30 <eladioquien@gmail.com>
2023-06-28 19:55:18 -0700

    Es todo por hoy

--- src/main/java/com/spring/SpringWEB/models/Usuario.java
+++ src/main/java/com/spring/SpringWEB/models/Usuario.java
@@ -1,25 +1,33 @@
 package com.spring.SpringWEB.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
-    @Getter @Setter
+
+    @Getter @Setter @Column(name = "id")
+    @Id
     private Long id;
 
-    @Getter @Setter
+    @Getter @Setter @Column(name = "nombre")
     private String nombre;
 
-    @Getter @Setter
+    @Getter @Setter @Column(name = "apellido")
     private String apellido;
 
-    @Getter @Setter
+    @Getter @Setter @Column(name = "email")
     private String email;
 
-    @Getter @Setter
+    @Getter @Setter @Column(name = "telefono")
     private String telefono;
 
-    @Getter @Setter
+    @Getter @Setter @Column(name = "password")
     private String password;
 
 }
